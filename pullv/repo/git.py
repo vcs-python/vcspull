2e7fc5a742fa31b5b296a579f51679bca9b6a154
Tony Narlock <tony@git-pull.com>
2013-10-06 13:29:40 +0800

    get progress on git clone

--- pullv/repo/git.py
+++ pullv/repo/git.py
@@ -32,12 +32,26 @@ class GitRepo(BaseRepo):
 
     def obtain(self):
         self.check_destination()
+        import subprocess
+        import sys
 
         url, rev = self.get_url_rev()
-        proc = run(
-            ['git', 'clone', '-q', url, self['path']],
+        self.info('Cloning')
+        process = subprocess.Popen(
+            ['git', 'clone', '--progress', url, self['path']],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
             env=os.environ.copy(), cwd=self['path']
         )
+        while True:
+            err = process.stderr.read(1)
+            if err == '' and process.poll() is not None:
+                break
+            if err != '':
+                sys.stderr.write(err)
+                sys.stderr.flush()
+
+        self.info('Cloned\n\t%s' % (process.stdout.read()))
 
     def update_repo(self):
         self.check_destination()
