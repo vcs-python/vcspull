c469b7cf3c3dd5d422bc49368f4b5700219c739a
Tony Narlock <tony@git-pull.com>
2013-11-24 19:17:03 -0600

    more work on test cli

--- pullv/testsuite/test_cli.py
+++ pullv/testsuite/test_cli.py
@@ -15,14 +15,50 @@ import tempfile
 import kaptan
 from pullv.repo import BaseRepo, Repo, GitRepo, MercurialRepo, SubversionRepo
 from pullv.util import expand_config, run, get_repos
-from .helpers import RepoTest
+from .helpers import RepoTest, ConfigTest
 
 logger = logging.getLogger(__name__)
 
 
-class ConfigLoadRepos(RepoTest):
+class LoadRepos(RepoTest, ConfigTest):
 
-    """Verify load_repos() load functionality."""
+    """Integration: Verify load_repos() load functionality.
+
+    Create a local svn, git and hg repo. Create YAML config file with paths.
+
+    Test load_repos arguments to filter.
+
+    """
+
+    def setUp(self):
+
+        ConfigTest.setUp(self)
+
+        self.git_repo_path, self.git_repo = self.create_git_repo()
+        self.hg_repo_path, self.hg_repo = self.create_mercurial_repo()
+        self.svn_repo_path, self.svn_repo = self.create_svn_repo()
+
+        config_yaml = """
+        {TMP_DIR}/study/:
+            linux: git+git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+            freebsd: git+https://github.com/freebsd/freebsd.git
+            sphinx: hg+https://bitbucket.org/birkenfeld/sphinx
+            docutils: svn+http://svn.code.sf.net/p/docutils/code/trunk
+        {TMP_DIR}/github_projects/:
+            kaptan:
+                repo: git+git@github.com:tony/kaptan.git
+                remotes:
+                    upstream: git+https://github.com/emre/kaptan
+                    marksteve: git+https://github.com/marksteve/kaptan.git
+        {TMP_DIR}:
+            .vim:
+                repo: git+git@github.com:tony/vim-config.git
+                shell_command_after: ln -sf /home/tony/.vim/.vimrc /home/tony/.vimrc
+            .tmux:
+                repo: git+git@github.com:tony/tmux-config.git
+                shell_command_after:
+                    - ln -sf /home/tony/.tmux/.tmux.conf /home/tony/.tmux.conf
+        """
 
     def test_load_repos(self):
         """Load all repos from all configs."""
