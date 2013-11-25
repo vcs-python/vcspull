8e092d184272059f409394b1dde2a712d0267179
Tony Narlock <tony@git-pull.com>
2013-11-24 21:11:36 -0600

    Initial test for #5

--- pullv/testsuite/test_config.py
+++ pullv/testsuite/test_config.py
@@ -9,9 +9,9 @@ pullv.tests.test_config
 
 """
 
-import logging
 import os
-import tempfile
+import copy
+import logging
 import kaptan
 from pullv.repo import BaseRepo, Repo, GitRepo, MercurialRepo, SubversionRepo
 from pullv.util import expand_config, run, get_repos
@@ -104,3 +104,64 @@ class ConfigExpandTest(ConfigExamples):
         config = expand_config(self.config_dict)
 
         self.assertDictEqual(config, self.config_dict_expanded)
+
+
+class ExpandUserExpandVars(ConfigTest):
+
+    """Verify .expandvars and .expanduser works with configs."""
+
+    def setUp(self):
+
+        ConfigTest.setUp(self)
+
+        config_yaml = """
+        '{TMP_DIR}/study/':
+            sphinx: hg+file://{hg_repo_path}
+            docutils: svn+file://{svn_repo_path}
+            linux: git+file://{git_repo_path}
+        '{TMP_DIR}/github_projects/':
+            kaptan:
+                repo: git+file://{git_repo_path}
+                remotes:
+                    test_remote: git+file://{git_repo_path}
+        '{TMP_DIR}':
+            .vim:
+                repo: git+file://{git_repo_path}
+            .tmux:
+                repo: git+file://{git_repo_path}
+        """
+
+        config_json = """
+        {
+          "${TMP_DIR}/study/": {
+            "sphinx": "hg+file://${hg_repo_path}",
+            "docutils": "svn+file://${svn_repo_path}",
+            "linux": "git+file://${git_repo_path}"
+          },
+          "${TMP_DIR}/github_projects/": {
+            "kaptan": {
+              "repo": "git+file://${git_repo_path}",
+              "remotes": {
+                "test_remote": "git+file://${git_repo_path}"
+              }
+            }
+          }
+        }
+        """
+
+        self.config_yaml = copy.deepcopy(config_yaml)
+
+        conf = kaptan.Kaptan(handler='yaml')
+        conf.import_config(self.config_yaml)
+        self.config1 = conf.export('dict')
+
+        self.config_json = copy.deepcopy(config_json)
+
+        conf = kaptan.Kaptan(handler='json')
+        conf.import_config(self.config_json)
+        self.config2 = conf.export('dict')
+
+    def testing_this(self):
+        config1_expanded = expand_config(self.config1)
+        config2_expanded = expand_config(self.config2)
+
