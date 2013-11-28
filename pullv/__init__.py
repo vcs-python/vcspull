d880ba7bb9ea31dcd4e6e362169cfd3135b9f899
Tony Narlock <tony@git-pull.com>
2013-11-28 04:14:09 -0600

    version above package imports, below __future__ statements.

--- pullv/__init__.py
+++ pullv/__init__.py
@@ -11,10 +11,11 @@ pullv
 """
 
 from __future__ import absolute_import, division, print_function, with_statement
+
+__version__ = '0.0.4'
+
 from .util import expand_config, get_repos, mkdir_p, run, scan, which
 from .log import LogFormatter, DebugLogFormatter, RepoLogFormatter
 from .cli import ConfigFileCompleter, command_load, find_configs, get_parser, \
     get_repos_new, in_dir, is_config_file, load_configs, main, scan_repos, \
     setup_logger, update, validate_schema
-
-__version__ = '0.0.4'
