b021e34e9a4f61c8e0e77aa7537857be364a03d2
Tony Narlock <tony@git-pull.com>
2013-11-28 04:13:09 -0600

    More python explicit imports to pullv package.

--- pullv/__init__.py
+++ pullv/__init__.py
@@ -13,6 +13,8 @@ pullv
 from __future__ import absolute_import, division, print_function, with_statement
 from .util import expand_config, get_repos, mkdir_p, run, scan, which
 from .log import LogFormatter, DebugLogFormatter, RepoLogFormatter
-from . import cli
+from .cli import ConfigFileCompleter, command_load, find_configs, get_parser, \
+    get_repos_new, in_dir, is_config_file, load_configs, main, scan_repos, \
+    setup_logger, update, validate_schema
 
 __version__ = '0.0.4'
