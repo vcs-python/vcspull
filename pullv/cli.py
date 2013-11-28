837e7f57afea30de8da758b6b8a4624d4336d4fa
Tony Narlock <tony@git-pull.com>
2013-11-28 05:24:36 -0600

    py3 iteritems => items

--- pullv/cli.py
+++ pullv/cli.py
@@ -247,7 +247,7 @@ def update(d, u):
 
     """
 
-    for k, v in u.iteritems():
+    for k, v in u.items():
         if isinstance(v, collections.Mapping):
             r = update(d.get(k, {}), v)
             d[k] = r
