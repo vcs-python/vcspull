d4f9343e1da491ebc595ce55ba92c4274e641804
Tony Narlock <tony@git-pull.com>
2016-05-23 14:56:12 -0500

    update asserts in test_config

--- tests/test_config.py
+++ tests/test_config.py
@@ -15,14 +15,15 @@ import tempfile
 import unittest
 
 import kaptan
+import pytest
 
 from vcspull import config, exc
 from vcspull.config import expand_config
-from .helpers import (ConfigTestCase, ConfigTestMixin, EnvironmentVarGuard,
-                      RepoIntegrationTest, assertConfigList)
 
-from .fixtures._util import loadfixture
 from .fixtures import example as fixtures
+from .fixtures._util import loadfixture
+from .helpers import (ConfigTestCase, ConfigTestMixin, EnvironmentVarGuard,
+                      RepoIntegrationTest)
 
 logger = logging.getLogger(__name__)
 
@@ -69,7 +70,7 @@ class ConfigImportExportTest(ConfigTestCase):
 
         new_config = kaptan.Kaptan()
         new_config_data = new_config.import_config(yaml_config_file).get()
-        self.assertDictEqual(self.config_dict, new_config_data)
+        assert self.config_dict == new_config_data
 
     def test_scan_config(self):
         configs = []
@@ -86,13 +87,13 @@ class ConfigImportExportTest(ConfigTestCase):
         files = 0
         if os.path.exists(os.path.join(self.TMP_DIR, '.vcspull.json')):
             files += 1
-            self.assertIn(os.path.join(self.TMP_DIR, '.vcspull.json'), configs)
+            assert os.path.join(self.TMP_DIR, '.vcspull.json') in configs
 
         if os.path.exists(os.path.join(self.TMP_DIR, '.vcspull.yaml')):
             files += 1
-            self.assertIn(os.path.join(self.TMP_DIR, '.vcspull.yaml'), configs)
+            assert os.path.join(self.TMP_DIR, '.vcspull.yaml') in configs
 
-        self.assertEqual(len(configs), files)
+        assert len(configs) == files
 
 
 class ConfigExpandTest(ConfigTestCase, unittest.TestCase):
@@ -101,11 +102,9 @@ class ConfigExpandTest(ConfigTestCase, unittest.TestCase):
 
     def test_expand_shell_command_after(self):
         """Expand shell commands from string to list."""
-        self.maxDiff = None
-
         config = expand_config(self.config_dict)
 
-        assertConfigList(config, self.config_dict_expanded)
+        assert config, self.config_dict_expanded
 
 
 class ExpandUserExpandVars(ConfigTestCase, ConfigTestMixin):
@@ -169,17 +168,14 @@ class ExpandUserExpandVars(ConfigTestCase, ConfigTestMixin):
         config2_expanded = expand_config(self.config2)
 
         paths = [r['parent_dir'] for r in config1_expanded]
-        self.assertIn(
-            os.path.expanduser(
-                os.path.expandvars('${HOME}/github_projects/')
-            ), paths
-        )
-        self.assertIn(os.path.expanduser('~/study/'), paths)
-        self.assertIn(os.path.expanduser('~'), paths)
+        assert os.path.expanduser(
+            os.path.expandvars('${HOME}/github_projects/')) in paths
+        assert os.path.expanduser('~/study/') in paths
+        assert os.path.expanduser('~') in paths
 
         paths = [r['parent_dir'] for r in config2_expanded]
-        self.assertIn(os.path.expandvars('${HOME}/github_projects/'), paths)
-        self.assertIn(os.path.expanduser('~/study/'), paths)
+        assert os.path.expandvars('${HOME}/github_projects/') in paths
+        assert os.path.expanduser('~/study/') in paths
 
 
 class InDirTest(ConfigTestCase):
@@ -191,7 +187,7 @@ class InDirTest(ConfigTestCase):
         self.CONFIG_DIR = os.path.join(self.TMP_DIR, '.vcspull')
 
         os.makedirs(self.CONFIG_DIR)
-        self.assertTrue(os.path.exists(self.CONFIG_DIR))
+        assert os.path.exists(self.CONFIG_DIR)
 
         self.config_file1 = tempfile.NamedTemporaryFile(
             dir=self.CONFIG_DIR, delete=False, suffix=".yaml"
@@ -212,10 +208,7 @@ class InDirTest(ConfigTestCase):
         ]
         result = config.in_dir(self.CONFIG_DIR)
 
-        try:
-            self.assertItemsEqual(expected, result)
-        except AttributeError:
-            self.assertCountEqual(expected, result)
+        assert len(expected) == len(result)
 
 
 class FindConfigsHome(ConfigTestCase, unittest.TestCase):
@@ -235,20 +228,20 @@ class FindConfigsHome(ConfigTestCase, unittest.TestCase):
 
         with EnvironmentVarGuard() as env:
             env.set("HOME", self.TMP_DIR)
-            self.assertEqual(os.environ.get("HOME"), self.TMP_DIR)
+            os.environ.get("HOME") == self.TMP_DIR
             expectedIn = os.path.join(self.TMP_DIR, '.vcspull.yaml')
             results = config.find_home_config_files()
 
-            self.assertIn(expectedIn, results)
+            assert expectedIn in results
 
     def test_multiple_configs_raises_exception(self):
         self.config_file2_path = os.path.join(self.TMP_DIR, '.vcspull.json')
         self.config_file2 = open(self.config_file2_path, 'a').close()
 
         with EnvironmentVarGuard() as env:
-            with self.assertRaises(exc.MultipleRootConfigs):
+            with pytest.raises(exc.MultipleRootConfigs):
                 env.set("HOME", self.TMP_DIR)
-                self.assertEqual(os.environ.get("HOME"), self.TMP_DIR)
+                assert os.environ.get("HOME") == self.TMP_DIR
                 config.find_home_config_files()
         os.remove(self.config_file2_path)
 
@@ -263,7 +256,7 @@ class FindConfigs(ConfigTestCase, unittest.TestCase):
         self.CONFIG_DIR = os.path.join(self.TMP_DIR, '.vcspull')
 
         os.makedirs(self.CONFIG_DIR)
-        self.assertTrue(os.path.exists(self.CONFIG_DIR))
+        assert os.path.exists(self.CONFIG_DIR)
 
         self.config_file1 = tempfile.NamedTemporaryFile(
             prefix="repos1",
@@ -285,14 +278,14 @@ class FindConfigs(ConfigTestCase, unittest.TestCase):
         """path as a string."""
         configs = config.find_config_files(path=self.CONFIG_DIR)
 
-        self.assertIn(self.config_file1.name, configs)
-        self.assertIn(self.config_file2.name, configs)
+        assert self.config_file1.name in configs
+        assert self.config_file2.name in configs
 
     def test_path_list(self):
         configs = config.find_config_files(path=[self.CONFIG_DIR])
 
-        self.assertIn(self.config_file1.name, configs)
-        self.assertIn(self.config_file2.name, configs)
+        assert self.config_file1.name in configs
+        assert self.config_file2.name in configs
 
     def test_match_string(self):
         configs = config.find_config_files(
@@ -300,52 +293,50 @@ class FindConfigs(ConfigTestCase, unittest.TestCase):
             match=self.config_file1_filename
         )
 
-        self.assertIn(self.config_file1.name, configs)
-        self.assertNotIn(self.config_file2.name, configs)
+        assert self.config_file1.name in configs
+        assert self.config_file2.name not in configs
 
         configs = config.find_config_files(
             path=[self.CONFIG_DIR],
             match=self.config_file2_filename
         )
 
-        self.assertNotIn(self.config_file1.name, configs)
-        self.assertIn(self.config_file2.name, configs)
+        assert self.config_file1.name not in configs
+        assert self.config_file2.name in configs
 
         configs = config.find_config_files(
             path=[self.CONFIG_DIR],
             match='randomstring'
         )
 
-        self.assertNotIn(self.config_file1.name, configs)
-        self.assertNotIn(self.config_file2.name, configs)
+        assert self.config_file1.name not in configs
+        assert self.config_file2.name not in configs
 
         configs = config.find_config_files(
             path=[self.CONFIG_DIR],
             match='*'
         )
 
-        self.assertIn(self.config_file1.name, configs)
-        self.assertIn(self.config_file2.name, configs)
+        assert self.config_file1.name in configs
+        assert self.config_file2.name in configs
 
         configs = config.find_config_files(
             path=[self.CONFIG_DIR],
             match='repos*'
         )
 
-        self.assertIn(self.config_file1.name, configs)
-        self.assertIn(self.config_file2.name, configs)
+        assert self.config_file1.name in configs
+        assert self.config_file2.name in configs
 
         configs = config.find_config_files(
             path=[self.CONFIG_DIR],
             match='repos[1-9]*'
         )
 
-        self.assertEqual(
-            len([c for c in configs if self.config_file1.name in c]), 1
-        )
+        assert len([c for c in configs if self.config_file1.name in c]) == 1
 
-        self.assertIn(self.config_file1.name, configs)
-        self.assertIn(self.config_file2.name, configs)
+        assert self.config_file1.name in configs
+        assert self.config_file2.name in configs
 
     def test_match_list(self):
         configs = config.find_config_files(
@@ -353,22 +344,18 @@ class FindConfigs(ConfigTestCase, unittest.TestCase):
             match=[self.config_file1_filename, self.config_file2_filename]
         )
 
-        self.assertIn(self.config_file1.name, configs)
-        self.assertIn(self.config_file2.name, configs)
+        assert self.config_file1.name in configs
+        assert self.config_file2.name in configs
 
         configs = config.find_config_files(
             path=[self.CONFIG_DIR],
             match=[self.config_file1_filename]
         )
 
-        self.assertIn(self.config_file1.name, configs)
-        self.assertEqual(
-            len([c for c in configs if self.config_file1.name in c]), 1
-        )
-        self.assertNotIn(self.config_file2.name, configs)
-        self.assertEqual(
-            len([c for c in configs if self.config_file2.name in c]), 0
-        )
+        assert self.config_file1.name in configs
+        assert len([c for c in configs if self.config_file1.name in c]) == 1
+        assert self.config_file2.name not in configs
+        assert len([c for c in configs if self.config_file2.name in c]) == 0
 
     def test_filetype_string(self):
         configs = config.find_config_files(
@@ -377,8 +364,8 @@ class FindConfigs(ConfigTestCase, unittest.TestCase):
             filetype='yaml',
         )
 
-        self.assertIn(self.config_file1.name, configs)
-        self.assertNotIn(self.config_file2.name, configs)
+        assert self.config_file1.name in configs
+        assert self.config_file2.name not in configs
 
         configs = config.find_config_files(
             path=[self.CONFIG_DIR],
@@ -386,8 +373,8 @@ class FindConfigs(ConfigTestCase, unittest.TestCase):
             filetype='json',
         )
 
-        self.assertNotIn(self.config_file1.name, configs)
-        self.assertNotIn(self.config_file2.name, configs)
+        assert self.config_file1.name not in configs
+        assert self.config_file2.name not in configs
 
         configs = config.find_config_files(
             path=[self.CONFIG_DIR],
@@ -395,8 +382,8 @@ class FindConfigs(ConfigTestCase, unittest.TestCase):
             filetype='json',
         )
 
-        self.assertNotIn(self.config_file1.name, configs)
-        self.assertIn(self.config_file2.name, configs)
+        assert self.config_file1.name not in configs
+        assert self.config_file2.name in configs
 
         configs = config.find_config_files(
             path=[self.CONFIG_DIR],
@@ -404,8 +391,8 @@ class FindConfigs(ConfigTestCase, unittest.TestCase):
             filetype='*',
         )
 
-        self.assertIn(self.config_file1.name, configs)
-        self.assertIn(self.config_file2.name, configs)
+        assert self.config_file1.name in configs
+        assert self.config_file2.name in configs
 
     def test_filetype_list(self):
         configs = config.find_config_files(
@@ -414,8 +401,8 @@ class FindConfigs(ConfigTestCase, unittest.TestCase):
             filetype=['*'],
         )
 
-        self.assertIn(self.config_file1.name, configs)
-        self.assertIn(self.config_file2.name, configs)
+        assert self.config_file1.name in configs
+        assert self.config_file2.name in configs
 
         configs = config.find_config_files(
             path=[self.CONFIG_DIR],
@@ -423,16 +410,16 @@ class FindConfigs(ConfigTestCase, unittest.TestCase):
             filetype=['json', 'yaml'],
         )
 
-        self.assertIn(self.config_file1.name, configs)
-        self.assertIn(self.config_file2.name, configs)
+        assert self.config_file1.name in configs
+        assert self.config_file2.name in configs
 
         configs = config.find_config_files(
             path=[self.CONFIG_DIR],
             filetype=['json', 'yaml'],
         )
 
-        self.assertIn(self.config_file1.name, configs)
-        self.assertIn(self.config_file2.name, configs)
+        assert self.config_file1.name in configs
+        assert self.config_file2.name in configs
 
     def test_include_home_configs(self):
         with EnvironmentVarGuard() as env:
@@ -443,8 +430,8 @@ class FindConfigs(ConfigTestCase, unittest.TestCase):
                 include_home=True
             )
 
-            self.assertIn(self.config_file1.name, configs)
-            self.assertIn(self.config_file2.name, configs)
+            assert self.config_file1.name in configs
+            assert self.config_file2.name in configs
 
             self.config_file3_path = os.path.join(
                 self.TMP_DIR, '.vcspull.json'
@@ -458,9 +445,9 @@ class FindConfigs(ConfigTestCase, unittest.TestCase):
             )
             expectedIn = os.path.join(self.TMP_DIR, '.vcspull.json')
 
-            self.assertIn(expectedIn, results)
-            self.assertIn(self.config_file1.name, results)
-            self.assertIn(self.config_file2.name, results)
+            assert expectedIn in results
+            assert self.config_file1.name in results
+            assert self.config_file2.name in results
 
             os.remove(self.config_file3_path)
 
@@ -540,18 +527,12 @@ class LoadConfigsUpdateDepth(RepoIntegrationDuplicateTest):
 
     def test_merge_nested_dict(self):
 
-        self.assertIn(
-            'vcsOn1',
+        assert 'vcsOn1' in \
             self.config3[os.path.join(self.TMP_DIR, 'srv/www/test/')]
-        )
-        self.assertNotIn(
-            'vcsOn2',
+        assert 'vcsOn2' not in \
             self.config3[os.path.join(self.TMP_DIR, 'srv/www/test/')]
-        )
-        self.assertIn(
-            'vcsOn2',
+        assert 'vcsOn2' in \
             self.config4[os.path.join(self.TMP_DIR, 'srv/www/test/')]
-        )
 
 
 class LoadConfigsDuplicate(RepoIntegrationDuplicateTest):
@@ -563,30 +544,12 @@ class LoadConfigsDuplicate(RepoIntegrationDuplicateTest):
             match="repoduplicate[1-2]"
         )
 
-        self.assertIn(self.config3_file, configs)
-        self.assertIn(self.config4_file, configs)
-        with self.assertRaises(Exception):
+        assert self.config3_file in configs
+        assert self.config4_file in configs
+        with pytest.raises(Exception):
             config.load_configs(configs)
 
     @unittest.skip("Not implemented")
     def test_duplicate_path_same_vcs(self):
         """Raise no warning if duplicate path same vcs."""
         pass
-
-
-def suite():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(ConfigExpandTest))
-    suite.addTest(unittest.makeSuite(ConfigFormatTest))
-    suite.addTest(unittest.makeSuite(ConfigImportExportTest))
-    suite.addTest(unittest.makeSuite(ExpandUserExpandVars))
-    suite.addTest(unittest.makeSuite(RepoIntegrationTest))
-    suite.addTest(unittest.makeSuite(FindConfigs))
-    suite.addTest(unittest.makeSuite(FindConfigsHome))
-    suite.addTest(unittest.makeSuite(LoadConfigs))
-    suite.addTest(unittest.makeSuite(InDirTest))
-    suite.addTest(unittest.makeSuite(RepoIntegrationDuplicateTest))
-    suite.addTest(unittest.makeSuite(LoadConfigsUpdateDepth))
-    suite.addTest(unittest.makeSuite(LoadConfigsDuplicate))
-
-    return suite
