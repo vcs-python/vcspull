360a501c8c0f172092253bed821e4163dfb51041
Tony Narlock <tony@git-pull.com>
2017-04-13 12:42:49 -0500

    add icon

--- doc/conf.py
+++ doc/conf.py
@@ -42,6 +42,7 @@ pygments_style = 'sphinx'
 import alabaster
 
 html_theme_path = [alabaster.get_path()]
+html_favicon = 'favicon.ico'
 html_theme = 'alabaster'
 html_theme_options = {
     'logo': 'img/vcspull.svg',
