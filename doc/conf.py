6136cc584a7807e60c584c7cedaf1c8b3155037a
Tony Narlock <tony@git-pull.com>
2017-05-26 13:28:10 -0500

    switch to alagitpull theme

--- doc/conf.py
+++ doc/conf.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
 
+import alagitpull
+
 # Get the project root dir, which is the parent dir of this
 cwd = os.getcwd()
 project_root = os.path.dirname(cwd)
@@ -15,7 +17,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'releases',
-    'alabaster',
+    'alagitpull',
 ]
 
 releases_unstable_prehistory = True
@@ -39,18 +41,21 @@ exclude_patterns = ['_build']
 
 pygments_style = 'sphinx'
 
-import alabaster
-
-html_theme_path = [alabaster.get_path()]
+html_theme_path = [alagitpull.get_path()]
 html_favicon = 'favicon.ico'
-html_theme = 'alabaster'
+html_theme = 'alagitpull'
 html_theme_options = {
     'logo': 'img/vcspull.svg',
+    'github_user': 'tony',
+    'github_repo': 'vcspull',
+    'github_type': 'star',
+    'github_banner': True,
+    'projects': alagitpull.projects,
+    'project_name': about['__title__'],
 }
 html_sidebars = {
     '**': [
         'about.html',
-        'star.html',
         'navigation.html',
         'relations.html',
         'more.html',
