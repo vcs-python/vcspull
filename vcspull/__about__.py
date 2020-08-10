301e5574f58b26c4c886f80cf27cfa9aa7cbf716
Tony Narlock <tony@git-pull.com>
2020-08-10 07:03:42 -0500

    build(metadata): Tweak description of project

--- vcspull/__about__.py
+++ vcspull/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'vcspull'
 __package_name__ = 'vcspull'
-__description__ = 'synchronize your repos and forks via declarative yaml/json file(s)'
+__description__ = 'synchronize your projects via yaml / json files'
 __version__ = '1.4.3'
 __author__ = 'Tony Narlock'
 __github__ = 'https://github.com/vcs-python/vcspull'
