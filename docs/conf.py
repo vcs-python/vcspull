d59aad3cb29f03be177953f6bbd55b48f115c200
Tony Narlock <tony@git-pull.com>
2022-02-26 07:55:56 -0600

    docs: Monokai theme for light too

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ release = "%s" % (about["__version__"])
 
 exclude_patterns = ["_build"]
 
-pygments_style = "sphinx"
+pygments_style = "monokai"
 pygments_dark_style = "monokai"
 
 html_favicon = "_static/favicon.ico"
