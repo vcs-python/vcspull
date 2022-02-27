63f17065f9dcd4d30adc3c81752223ce8c9dd55d
Tony Narlock <tony@git-pull.com>
2022-02-27 05:38:12 -0600

    test(config): New helper fixture: load_yaml

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,19 +1,32 @@
+import pytest
+
 from vcspull import config
 
 
-def test_relative_dir(tmpdir):
-    arbitrary_dir = tmpdir.join("moo")
-    arbitrary_dir.mkdir()
+@pytest.fixture
+def load_yaml(tmpdir):
+    def fn(content, dir="randomdir", filename="randomfilename.yaml"):
+        _dir = tmpdir.join(dir)
+        _dir.mkdir()
+        _dir.join(filename).write(content)
 
-    arbitrary_dir.join("rel.yaml").write(
+        configs = config.find_config_files(path=str(_dir))
+        repos = config.load_configs(configs, str(_dir))
+        return _dir, configs, repos
+
+    return fn
+
+
+def test_relative_dir(load_yaml):
+    dir, _, repos = load_yaml(
         """
 ./relativedir:
   docutils: svn+http://svn.code.sf.net/p/docutils/code/trunk
    """
     )
 
-    configs = config.find_config_files(path=str(arbitrary_dir))
-    repos = config.load_configs(configs, str(arbitrary_dir))
+    configs = config.find_config_files(path=str(dir))
+    repos = config.load_configs(configs, str(dir))
 
-    assert str(arbitrary_dir.join("relativedir")) == repos[0]["parent_dir"]
-    assert str(arbitrary_dir.join("relativedir", "docutils")) == repos[0]["repo_dir"]
+    assert str(dir.join("relativedir")) == repos[0]["parent_dir"]
+    assert str(dir.join("relativedir", "docutils")) == repos[0]["repo_dir"]
