457fafe1fabbc5d142f5d4ac8a5f83371f440233
Tony Narlock <tony@git-pull.com>
2022-03-12 10:31:48 -0600

    refactor(tests): Port latest config and path changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
+import getpass
 import pathlib
+import shutil
+import textwrap
 
 import pytest
 
@@ -7,38 +10,55 @@ from libvcs.shortcuts import create_repo_from_pip_url
 from libvcs.util import run
 
 
+@pytest.fixture(autouse=True, scope="session")
+def home_path(tmp_path_factory: pytest.TempPathFactory):
+    return tmp_path_factory.mktemp("home")
+
+
+@pytest.fixture(autouse=True, scope="session")
+def user_path(home_path: pathlib.Path):
+    p = home_path / getpass.getuser()
+    p.mkdir()
+    return p
+
+
 @pytest.fixture(scope="function")
-def tmpdir_repoparent(tmp_path: pathlib.Path):
+def repos_path(user_path: pathlib.Path, request: pytest.FixtureRequest):
     """Return temporary directory for repository checkout guaranteed unique."""
-    fn = tmp_path
-    return fn
+    dir = user_path / "repos"
+    dir.mkdir(exist_ok=True)
+
+    def clean():
+        shutil.rmtree(dir)
+
+    request.addfinalizer(clean)
+    return dir
 
 
 @pytest.fixture
-def git_repo_kwargs(tmpdir_repoparent: pathlib.Path, git_dummy_repo_dir):
+def git_repo_kwargs(repos_path: pathlib.Path, git_dummy_repo_dir):
     """Return kwargs for :func:`create_repo_from_pip_url`."""
-    repo_name = "repo_clone"
     return {
         "url": "git+file://" + git_dummy_repo_dir,
-        "parent_dir": str(tmpdir_repoparent),
-        "name": repo_name,
+        "parent_dir": str(repos_path),
+        "name": "repo_name",
     }
 
 
 @pytest.fixture
 def git_repo(git_repo_kwargs) -> GitRepo:
     """Create an git repository for tests. Return repo."""
-    git_repo = create_repo_from_pip_url(**git_repo_kwargs)
-    git_repo.obtain(quiet=True)
-    return git_repo
+    repo = create_repo_from_pip_url(**git_repo_kwargs)
+    repo.obtain(quiet=True)
+    return repo
 
 
 @pytest.fixture
-def create_git_dummy_repo(tmpdir_repoparent: pathlib.Path) -> pathlib.Path:
+def create_git_dummy_repo(repos_path: pathlib.Path) -> pathlib.Path:
     def fn(repo_name, testfile_filename="testfile.test"):
-        repo_path = str(tmpdir_repoparent / repo_name)
+        repo_path = str(repos_path / repo_name)
 
-        run(["git", "init", repo_name], cwd=str(tmpdir_repoparent))
+        run(["git", "init", repo_name], cwd=str(repos_path))
 
         run(["touch", testfile_filename], cwd=repo_path)
         run(["git", "add", testfile_filename], cwd=repo_path)
@@ -50,20 +70,53 @@ def create_git_dummy_repo(tmpdir_repoparent: pathlib.Path) -> pathlib.Path:
 
 
 @pytest.fixture
-def git_dummy_repo_dir(tmpdir_repoparent: pathlib.Path, create_git_dummy_repo):
+def git_dummy_repo_dir(repos_path: pathlib.Path, create_git_dummy_repo):
     """Create a git repo with 1 commit, used as a remote."""
     return create_git_dummy_repo("dummyrepo")
 
 
 @pytest.fixture(scope="function")
-def home_path(tmp_path: pathlib.Path, monkeypatch: pytest.MonkeyPatch) -> pathlib.Path:
-    monkeypatch.setenv("HOME", str(tmp_path))
-    tmp_path.mkdir(exist_ok=True)
-    return tmp_path
-
-
-@pytest.fixture(scope="function")
-def config_path(home_path: pathlib.Path):
+def config_path(home_path: pathlib.Path, request: pytest.FixtureRequest):
     conf_path = home_path / ".vcspull"
-    conf_path.mkdir()
+    conf_path.mkdir(exist_ok=True)
+
+    def clean():
+        shutil.rmtree(conf_path)
+
+    request.addfinalizer(clean)
     return conf_path
+
+
+@pytest.fixture(autouse=True, scope="session")
+def hgrc(user_path: pathlib.Path):
+    hgrc = user_path / ".hgrc"
+    hgrc.write_text(
+        textwrap.dedent(
+            f"""
+        [ui]
+        username = vcspull tests <vcspull@git-pull.com>
+        merge = internal:merge
+
+        [trusted]
+        users = {getpass.getuser()}
+    """
+        ),
+        encoding="utf-8",
+    )
+    return hgrc
+
+
+@pytest.fixture(autouse=True, scope="module")
+def gitconfig(user_path: pathlib.Path):
+    gitconfig = user_path / ".gitconfig"
+    gitconfig.write_text(
+        textwrap.dedent(
+            f"""
+  [user]
+    email = libvcs@git-pull.com
+    name = {getpass.getuser()}
+    """
+        ),
+        encoding="utf-8",
+    )
+    return gitconfig
