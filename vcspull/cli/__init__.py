167c5fb8e1aa09d8fb4b9230bcf6ad2ea0397523
Tony Narlock <tony@git-pull.com>
2022-04-10 04:54:27 -0500

    feat(version): Show libvcs version with --version

--- vcspull/cli/__init__.py
+++ vcspull/cli/__init__.py
@@ -8,6 +8,8 @@ import logging
 
 import click
 
+from libvcs.__about__ import __version__ as libvcs_version
+
 from ..__about__ import __version__
 from ..log import setup_logger
 from .sync import sync
@@ -21,7 +23,12 @@ log = logging.getLogger(__name__)
     default="INFO",
     help="Log level (DEBUG, INFO, WARNING, ERROR, CRITICAL)",
 )
-@click.version_option(__version__, "-V", "--version", message="%(prog)s %(version)s")
+@click.version_option(
+    __version__,
+    "-V",
+    "--version",
+    message=f"%(prog)s %(version)s, libvcs {libvcs_version}",
+)
 def cli(log_level):
     setup_logger(log=log, level=log_level.upper())
 
