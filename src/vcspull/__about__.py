692adcfa8b0a8fd4ab3988412e7c93f8d7228446
Tony Narlock <tony@git-pull.com>
2022-09-25 20:59:53 -0500

    Tag v1.13.0a17 (the final alpha? Includes libvcs 0.17.0a1)

--- src/vcspull/__about__.py
+++ src/vcspull/__about__.py
@@ -1,7 +1,7 @@
 __title__ = "vcspull"
 __package_name__ = "vcspull"
 __description__ = "Manage and sync multiple git, mercurial, and svn repos"
-__version__ = "1.13.0a16"
+__version__ = "1.13.0a17"
 __author__ = "Tony Narlock"
 __github__ = "https://github.com/vcs-python/vcspull"
 __docs__ = "https://vcspull.git-pull.com"
