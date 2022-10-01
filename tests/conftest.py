94db3b0d4b5eef70dc1f226006f23263228e4b01
Tony Narlock <tony@git-pull.com>
2022-10-01 11:02:57 -0500

    refactor(conftest): Remove local repo fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,8 @@ import getpass
 import pathlib
 import shutil
 import textwrap
-import typing as t
 
 import pytest
-
-from libvcs._internal.run import run
-from libvcs._internal.shortcuts import create_project
-from libvcs.sync.git import GitSync
 
 
 @pytest.fixture(autouse=True, scope="session")
@@ -64,50 +59,6 @@ def repos_path(user_path: pathlib.Path, request: pytest.FixtureRequest):
 
     request.addfinalizer(clean)
     return dir
-
-
-@pytest.fixture
-def git_repo_kwargs(repos_path: pathlib.Path, git_dummy_repo_dir):
-    """Return kwargs for :func:`create_project`."""
-    return {
-        "url": "git+file://" + git_dummy_repo_dir,
-        "dir": str(repos_path / "repo_name"),
-        "name": "repo_name",
-    }
-
-
-class DummyRepoProtocol(t.Protocol):
-    """Callback for repo fixture factory."""
-
-    def __call__(self, repo_name: str, testfile_filename: str = ...) -> str:
-        """Callback signature for subprocess communication."""
-        ...
-
-
-@pytest.fixture
-def create_git_dummy_repo(
-    repos_path: pathlib.Path,
-) -> t.Generator[DummyRepoProtocol, None, None]:
-    def fn(repo_name: str, testfile_filename: str = "testfile.test"):
-        repo_path = str(repos_path / repo_name)
-
-        run(["git", "init", repo_name], cwd=str(repos_path))
-
-        run(["touch", testfile_filename], cwd=repo_path)
-        run(["git", "add", testfile_filename], cwd=repo_path)
-        run(["git", "commit", "-m", "test file for %s" % repo_name], cwd=repo_path)
-
-        return repo_path
-
-    yield fn
-
-
-@pytest.fixture
-def git_dummy_repo_dir(
-    repos_path: pathlib.Path, create_git_dummy_repo: DummyRepoProtocol
-):
-    """Create a git repo with 1 commit, used as a remote."""
-    return create_git_dummy_repo("dummyrepo")
 
 
 @pytest.fixture(autouse=True, scope="session")
