2d1b714e382bad46503d812d16ab379132ec0fd9
Tony Narlock <tony@git-pull.com>
2023-12-17 06:46:42 -0600

    docs(exc): Note pydocstyle updates

--- src/vcspull/exc.py
+++ src/vcspull/exc.py
@@ -1,6 +1,11 @@
+"""Exceptions for vcspull."""
+
+
 class VCSPullException(Exception):
     """Standard exception raised by vcspull."""
 
 
 class MultipleConfigWarning(VCSPullException):
+    """Multiple eligible config files found at the same time."""
+
     message = "Multiple configs found in home directory use only one." " .yaml, .json."
