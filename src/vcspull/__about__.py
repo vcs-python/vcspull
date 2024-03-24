7297ba540a90aff51feb827089bb0de19aa02c4e
Tony Narlock <tony@git-pull.com>
2024-03-24 12:51:39 -0500

    Tag v1.29.0 (libvcs 0.29.0)

--- src/vcspull/__about__.py
+++ src/vcspull/__about__.py
@@ -3,7 +3,7 @@
 __title__ = "vcspull"
 __package_name__ = "vcspull"
 __description__ = "Manage and sync multiple git, mercurial, and svn repos"
-__version__ = "1.28.1"
+__version__ = "1.29.0"
 __author__ = "Tony Narlock"
 __github__ = "https://github.com/vcs-python/vcspull"
 __docs__ = "https://vcspull.git-pull.com"
